104935251b727d1afa6acc002c4346494e06b9f1
nehamenon704 <nehamenon704@gmail.com>
2024-04-07 12:17:34 -0700

    made file for documentation

--- tests/settingup.py
+++ tests/settingup.py
@@ -10,8 +10,10 @@ data= pd.DataFrame(data_for_df)
 file1= open("test1.csv", "w")
 file1.write("this is a test file")
 
-
+file2= open("testing_data.csv", "w")
+file2.write("need to put")
 data.to_csv('targ.csv', index=False)
+
 
 
 #make a empty zip file
@@ -22,3 +24,4 @@ with zipfile.ZipFile('dummydata.zip', 'w', zipfile.ZIP_STORED) as zipf:
     # zipf.write('test1.csv')
     zipf.write("targ.csv")
     zipf.write("test1.csv")
+    zipf.write("testing_data.csv")
